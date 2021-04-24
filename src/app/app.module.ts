b4001c7c8caae5954e84ffbdf879cd17fb14cfc1
Phoebe Pan <phoebe.pan@ytml.com.au>
2021-04-24 13:59:51 +1000

    init page & hook post API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post/post.component.ts
@@ -0,0 +1,30 @@
+import { PostService } from './../../post.service';
+import { Article } from './../../interfaces/article';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  article: Article;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('id')),
+      switchMap(id => this.postService.getArticle(id))
+    ).subscribe(result => {
+      this.article = result.article;
+    });
+  }
+
+}
